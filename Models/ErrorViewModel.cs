d8af3f82e6020e334b616e61eb56be59e7f03527
Anjali-st <anjali90908080@gmail.com>
2023-09-13 20:06:51 +0530

    PartialViews

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sec_04Partial__Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
